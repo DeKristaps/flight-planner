4e79829cfaeb3d194f5506076124398548ffa018
DeKristaps <dekristaps@gmail.com>
2022-10-25 22:19:12 +0300

    Created an empty flight-planner project

---
+++ src/main/java/io/codelex/flightplanner/FlightPlannerApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.flightplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codelex/flightplanner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codelex.flightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
